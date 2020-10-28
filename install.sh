6742f5b08c7aa35ef8cff5d6f17d03845f8aeb66
Kurt J. Schoener <kjschoener@gmail.com>
2020-10-28 17:03:05 -0400

    Add docker protections
    > Add comments about docker-ce being enabled by default
    > Add commented command to disable docker if that's what we want
    > Only run containers if docker is running

--- install.sh
+++ install.sh
@@ -90,22 +90,27 @@ echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' |
 apt update || exit 22
 apt remove docker docker-engine docker.io || exit 23
 apt install -y docker-ce || exit 24
+# The vendor preset for docker-ce is for the service to be enabled.
+# If we do not want it to run on boot, we need to disable it
+# systemctl disable docker || exit 40
 
 # setup containers
-docker run --name webgoat -d --restart unless-stopped -p 8881:8080 -t santosomar/webgoat
-docker run --name juice-shop --restart unless-stopped -d -p 8882:3000 santosomar/juice-shop:latest
-docker run --name dvwa --restart unless-stopped -itd -p 8883:80 santosomar/dvwa
-docker run --name mutillidae_2 --restart unless-stopped -d -p 8884:80 santosomar/mutillidae_2
-docker run --name bwapp2 --restart unless-stopped -d -p 8885:80 santosomar/bwapp
-docker run --name dvna --restart unless-stopped -d -p 8886:9090 santosomar/dvna
-docker run --name hackazon -d --restart unless-stopped -p 8887:80 santosomar/hackazon
-docker run --name hackme-rtov -d --restart unless-stopped -p 8888:80 santosomar/hackme-rtov
-docker run --name mayhem -d --restart unless-stopped -p 8889:80 -p 88:22 santosomar/mayhem
-docker run --name rtv-safemode -d --restart unless-stopped -p 9000:80 -p 3306:3306 santosomar/rtv-safemode
-docker run --name grayhat-mmxx -d --restart unless-stopped -p 9001:8001 santosomar/grayhat-mmxx
-docker run --name yascon-hackme -d --restart unless-stopped -p 9002:80 santosomar/yascon-hackme
-
-# --> Docker seems to be starting at boot.  Check why.
+if sudo docker ps 2>&1 1>/dev/null; then
+    docker run --name webgoat -d --restart unless-stopped -p 8881:8080 -t santosomar/webgoat
+    docker run --name juice-shop --restart unless-stopped -d -p 8882:3000 santosomar/juice-shop:latest
+    docker run --name dvwa --restart unless-stopped -itd -p 8883:80 santosomar/dvwa
+    docker run --name mutillidae_2 --restart unless-stopped -d -p 8884:80 santosomar/mutillidae_2
+    docker run --name bwapp2 --restart unless-stopped -d -p 8885:80 santosomar/bwapp
+    docker run --name dvna --restart unless-stopped -d -p 8886:9090 santosomar/dvna
+    docker run --name hackazon -d --restart unless-stopped -p 8887:80 santosomar/hackazon
+    docker run --name hackme-rtov -d --restart unless-stopped -p 8888:80 santosomar/hackme-rtov
+    docker run --name mayhem -d --restart unless-stopped -p 8889:80 -p 88:22 santosomar/mayhem
+    docker run --name rtv-safemode -d --restart unless-stopped -p 9000:80 -p 3306:3306 santosomar/rtv-safemode
+    docker run --name grayhat-mmxx -d --restart unless-stopped -p 9001:8001 santosomar/grayhat-mmxx
+    docker run --name yascon-hackme -d --restart unless-stopped -p 9002:80 santosomar/yascon-hackme
+else
+    echo "[ERROR] Docker is not running. Cannot run containers."
+fi
 # for bwapp - go to /install.php then user/pass is bee/bug
 
 #downloading the h4cker wallpaper
@@ -129,7 +134,6 @@ source .bashrc
 #     sense of security.
 
 #Getting the container info script
-# --> This should go in the ethical-hacking dir 
 wget http://websploit.h4cker.org/containers.sh
 chmod 744 containers.sh
 
